9fe803fb998cdcd014798ad4cf69c250f446ae59
Eduardo <eduardo.weissheimer22@gmail.com>
2025-05-05 06:17:03 -0300

    Base projeto

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Banco_Laravel
-Projeto de banco utilizando Laravel
+# Requisitos:
+* PHP 8.2 ou superior - Conferir a versão: 
+> php -v
+
+* Composer - Conferir a instalação:
+> composer --version
+
+* Node.js 22 ou superior - Conferir a versão:
+> node -v
+
+# Autor:
+
+Esse projeto foi desenvolvido por [Eduardo Weissheimer](https://github.com/Eduardo220), e está hospedado no repositório [Banco-Laravel](https://github.com/Eduardo220).
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Banco</title>
+    </head>
+    <body>
+        <div id="app">
+            <h1>Banco</h1>
+            <p>Welcome to the Banco application!</p>
+        </div>
+    </body>
+    <footer>
+        <p>&copy; 2025 Banco. All rights reserved.</p>
+        <p>Banco is a fictional bank application.</p>
+    </footer>    
+</html>
