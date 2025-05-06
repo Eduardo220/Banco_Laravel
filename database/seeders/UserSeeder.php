d6845d6b16ef306ca4c9ba14954c66346e832fcf
Eduardo <eduardo.weissheimer22@gmail.com>
2025-05-06 07:56:15 -0300

    atualização no seeders

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,35 +19,61 @@ class UserSeeder extends Seeder
                 'name' => 'Eduardo',
                 'email' => 'eduardo@gmail.com',
                 'password' => '123456#A',
+                'phone' => '11999999999',
+                'address' => 'Rua A, 123',
+                'CPF' => '12345678900',
+                'birth_date' => '2004-03-03',
             ]); 
         }
-
+        if (!User::where('email', 'fernanda@gmail.com')->first()) 
+        {
+            User::create([
+                'name' => 'Fernanda',
+                'email' => 'fernanda@gmail.com',
+                'password' => '123456#A',
+                'phone' => '11999999999',
+                'address' => 'Rua A, 123',
+                'CPF' => '11345678900',
+                'birth_date' => '2009-10-25',
+            ]); 
+        }
         if (!User::where('email', 'gabriela@gmail.com')->first()) 
         {
             User::create([
                 'name' => 'Gabriela',
                 'email' => 'gabriela@gmail.com',
                 'password' => '123456#A',
+                'phone' => '11999999999',
+                'address' => 'Rua A, 123',
+                'CPF' => '12145678900',
+                'birth_date' => '2004-11-03',
             ]); 
         }
-
+        if (!User::where('email', 'roger@gmail.com')->first()) 
+        {
+            User::create([
+                'name' => 'Roger',
+                'email' => 'roger@gmail.com',
+                'password' => '123456#A',
+                'phone' => '11999999999',
+                'address' => 'Rua A, 123',
+                'CPF' => '12315678900',
+                'birth_date' => '1969-08-18',
+            ]); 
+        }
         if (!User::where('email', 'daniel@gmail.com')->first()) 
         {
             User::create([
                 'name' => 'Daniel',
                 'email' => 'daniel@gmail.com',
                 'password' => '123456#A',
-            ]); 
-        }
-
-        if (!User::where('email', 'marilei@gmail.com')->first()) 
-        {
-            User::create([
-                'name' => 'Marilei',
-                'email' => 'marilei@gmail.com',
-                'password' => '123456#A',
+                'phone' => '11999999999',
+                'address' => 'Rua A, 123',
+                'CPF' => '12341678900',
+                'birth_date' => '1990-01-01',
             ]); 
         }
     }
 }
+
 
