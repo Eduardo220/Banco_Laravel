d6d495ea03ef729d339fb5cc359607f24f43e8f3
Eduardo <eduardo.weissheimer22@gmail.com>
2025-05-07 13:41:18 -0300

    atualizando registros do cadastro

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -32,9 +32,9 @@ class RegisterController extends Controller
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'phone' => 'required|string|max:15',
+            'phone' => 'required|string|min:11|max:11',
             'address' => 'required|string|max:255',
-            'CPF' => 'required|string|max:14|unique:users',
+            'CPF' => 'required|string|min:11|max:11|unique:users',
             'birth_date' => 'required|date',
             'password' => 'required|string|min:8|confirmed',
         ]);
--- resources/views/users/register/index.blade.php
+++ resources/views/users/register/index.blade.php
@@ -15,13 +15,13 @@
                 <input type="email" name="email" id="email" placeholder="Insira seu email" required><br><br>
 
                 <label for="phone">Celular: </label>
-                <input type="text" name="phone" id="phone" placeholder="Insira seu número" required><br><br>
+                <input type="text" name="phone" id="phone" minlength="11" maxlength="11" placeholder="(XX)XXXXX-XXXX" required><br><br>
 
                 <label for="address">Endereço: </label>
                 <input type="text" name="address" id="address" placeholder="Insira seu endereço" required><br><br>
 
                 <label for="CPF">CPF: </label>
-                <input type="int" name="CPF" id="CPF" maxlength="14" placeholder="Insira seu CPF" required><br><br>
+                <input type="int" name="CPF" id="CPF" minlength="11" maxlength="11" placeholder="Insira seu CPF" required><br><br>
 
                 <label for="birth_date">Data de Nascimento: </label>
                 <input type="date" name="birth_date" id="birth_date" required><br><br>
